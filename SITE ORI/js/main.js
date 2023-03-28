1ec3972d02a36a303a56ec608fd6ae748f038d04
Fused Labs <129000419+FusedLabsSL@users.noreply.github.com>
2023-03-28 14:42:55 +0530

    Delete SITE ORI directory

--- SITE ORI/js/main.js
+++
@@ -1,9 +0,0 @@
-
-// navbar
-window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-  });
-
-
-
