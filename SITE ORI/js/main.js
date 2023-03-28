90d43a6cc1806a28af931d7a06ab336f06782cea
Fused Labs <129000419+FusedLabsSL@users.noreply.github.com>
2023-03-28 14:26:55 +0530

    Add files via upload

---
+++ SITE ORI/js/main.js
@@ -0,0 +1,9 @@
+
+// navbar
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+
+
+
